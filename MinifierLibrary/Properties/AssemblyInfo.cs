25a399da369e9e6cb1033cc9d2dc1ad26f2f8419
msusur <mail@mertsusur.com>
2013-12-05 13:25:12 +0200

    nuget update and nuget package updated to v.1.0.2

--- MinifierLibrary/Properties/AssemblyInfo.cs
+++ MinifierLibrary/Properties/AssemblyInfo.cs
@@ -2,9 +2,9 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("MinifierLibrary")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Basic html output minifier for Razor View Engine. Based on work of https://github.com/meleze/Meleze.Web")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Mert.Susur")]
 [assembly: AssemblyProduct("MinifierLibrary")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("46ad4b3f-29b2-4165-bd31-752ee3c685ad")]
 
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
---
+++ create-nuget-package.bat
@@ -0,0 +1,1 @@
+.nuget\nuget pack MinifierLibrary\MinifierLibrary.csproj -Build -Properties Configuration=Release -version 1.0.2
